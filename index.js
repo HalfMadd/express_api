5cc3e48e86eb9dcbb5c2eba766d33ab6088180a2
HalfMadd <remilacuisse@gmail.com>
2020-04-03 16:59:02 +0200

    fin semaine 1

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const v1 = express.Router();
 require('dotenv').config();
 
 const basicAuth = require('./middleware/basic-auth').basicAuth;
+
 const MessageService = require('./services/message-service');
 const messageService = new MessageService;
+
+const FileService = require('./services/file-service');
+const fileService = new FileService();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -55,6 +59,74 @@ v1.delete('/message/:id', basicAuth, async (request, response) => {
         response.sendStatus(400);
     }
 });
+
+v1.put('/message/:id', basicAuth, async (request, response) => {
+    const id = request.params.id;
+    const message = request.body;
+    if (!MessageService.isValid(message)) return sendStatus(400);
+    try {
+        const result = await messageService.updateMessage(message, id);
+        if(!result.isFind) return response.sendStatus(404);
+        result.isModified ? response.sendStatus(200) : response.sendStatus(304);
+    } catch(e){
+        console.log('error occurs : ', e);
+        response.sendStatus(400);
+    }
+});
+
+const multer = require('multer');
+const upload = multer({ dest: 'data/upload/' });
+
+v1.post('/file', upload.single('myFile'), (request, response) => {
+    try {
+        await fileService.saveFileInfos(request.file);
+        response.sendStatus(200);
+    }catch(e){
+        response.sendStatus(500);
+    }
+});
+
+v1.get('/file', async (request, response) => {
+    try {
+        const filesInfo = await fileService.getFilesInfo();
+        response.send(filesInfo);
+    } catch(e) {
+        console.log('error occurs ', e);
+        response.sendStatus(500);
+    }
+});
+
+v1.get('/file/:id', async (request, response) => {
+    const id = request.params.id;
+    try{
+        const file = await fileService.getFilesInfo(id);
+        if(file) {
+            response.setHeader(
+                'Content-disposition',
+                'attachment: filename=' + fileresult.fileInfo['original-name']
+            );
+            response.setHeader('Content-type', fileResult.fileInfo['mime-type']);
+            response.setHeader('Content-length', fileResult.fileInfo.size);
+            fileResult.file.pipe(response);
+        }else{
+            response.sendStatus(404);
+        }
+    } catch(e){
+        console.log('error occurs ', e);
+        response.sendStatus(500);
+    }
+});
+
+v1.delete('/file/:id', async (request, response) => {
+    const id = request.params.id;
+    try{
+        await fileService.deleteFile(id);
+        response.sendStatus(200);
+    }catch(e){
+        console.log('error occurs ', e);
+        response.sendStatus(500);
+    }
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
--- services/message-service.js
+++ services/message-service.js
@@ -1,6 +1,12 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
 module.exports = class MessageService {
+
+    static isMessageValid(message){
+        return message.quote && message.quote.length >0
+            && message.author && message.author.length >0;
+    }
+
     getConnectedClient(){
         const client = new MongoClient(
             process.env.MONGO_CONNECTION_URL,
@@ -55,6 +61,26 @@ module.exports = class MessageService {
 
         await client.close();
         return result.deletedCount === 1;
+    }
+
+    async updateMessage(message, id){
+        const client = await this.getConnectedClient();
+        const collection = client.db(process.env.MONGO_DB).collection('messages');
+
+        const query = {
+            _id: ObjectID(id)
+        }
+        const updateQuery = {
+            $set: message
+        }
+        collection.updateOne(query, updateQuery);
+
+        await client.close();
+
+        return {
+            isFind: result.matchedCount == 1,
+            isModified: result.modifiedCount == 1
+        }
     }
 
 
